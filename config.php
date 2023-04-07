4622b8bbe0240d4135701fb173bc961781b73af0
rannie0211 <129827841+rannie0211@users.noreply.github.com>
2023-04-07 20:21:17 +0800

    connect to azure

--- config.php
+++ config.php
@@ -1,5 +1,74 @@
 <?php
+    $servername = "budgeting.mysql.database.azure.com";
 
-echo "Hello World! Second change to change this "
+    $dbusername = "rannie";
+    $dbpassword = "990211Rouyi";
+    $dbname = "budgeting-db";
 
+    //Initializes MySQLi
+    $conn = mysqli_init();
+
+    mysqli_ssl_set($conn,NULL,NULL, "/var/www/html/DigiCertGlobalRootG2.crt.pem", NULL, NULL);
+
+    // Establish the connection
+    mysqli_real_connect($conn, 'budgeting.mysql.database.azure.com', 'rannie', '990211Rouyi', 'budgeting-db', 3306, NULL, MYSQLI_CLIENT_SSL);
+
+    //If connection failed, show the error
+    if (mysqli_connect_errno())
+    {
+        die('Failed to connect to MySQL: '.mysqli_connect_error());
+    }
+
+    function showMessage($status, $title, $message, $url)
+    {
+        $final = 
+        array(
+            "status" => $status,
+            "title" => $title,
+            "message" => $message,
+            "url" => $url
+        );
+        $finalJson = json_encode($final);
+        echo $finalJson;
+    }
+
+    function generateRandomString($length = 5)
+    {
+        $characters = '0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ';
+        $charactersLength = strlen($characters);
+        $randomString = '';
+        for ($i = 0; $i < $length; $i++) {
+            $randomString .= $characters[rand(0, $charactersLength - 1)];
+        }
+        return $randomString;
+    }
+
+    function generateRandomNumber($length = 5)
+    {
+        $characters = '0123456789';
+        $charactersLength = strlen($characters);
+        $randomString = '';
+        for ($i = 0; $i < $length; $i++) {
+            $randomString .= $characters[rand(0, $charactersLength - 1)];
+        }
+        return $randomString;
+    }
+
+    function getDatesFromRange($start, $end, $format = 'Y-m-d') 
+    {
+        $array = array();
+        $interval = new DateInterval('P1D');
+
+        $realEnd = new DateTime($end);
+        $realEnd->add($interval);
+
+        $period = new DatePeriod(new DateTime($start), $interval, $realEnd);
+
+        foreach($period as $date)
+        { 
+            $array[] = $date->format($format); 
+        }
+
+        return $array;
+    }
 ?>
---
+++ user_register.php
@@ -0,0 +1,62 @@
+<?php
+
+include('config.php');
+
+$username = "";
+$email = "";
+$password = "";
+$loginToken = generateRandomString();
+
+
+if (!empty($_GET))
+{
+	$username = $_GET["username"];
+    $email = $_GET["email"];
+	$password = $_GET["password"];
+}
+	
+if (!empty($_POST))
+{
+	$username = $_POST["username"];
+    $email = $_POST["email"];
+	$password = $_POST["password"];
+}
+	
+if ($username === "" || $email === "" || $password === "")
+{
+	showMessage(0, "Failed", "Invalid request.", "");
+    return;
+}
+
+// Create connection
+$conn = new mysqli($servername, $dbusername, $dbpassword, $dbname);
+
+// Check connection
+if ($conn->connect_error)
+{
+	showMessage(0, "Failed", "Connection failed: " . $conn->connect_error, "");
+}
+	
+$conn->query('set character_set_client=utf8');
+$conn->query('set character_set_connection=utf8');
+$conn->query('set character_set_results=utf8');
+$conn->query('set character_set_server=utf8');
+
+$sql = "INSERT INTO user (user_username, user_email, user_password) VALUES ('". $username ."', '". $email ."', '". $password ."')";
+
+$result = $conn->query($sql);
+
+if($result)
+{
+    showMessage(1, "Success", "The account is registered successfully!","");
+    return;
+}
+else
+{
+    showMessage(0, "Error", "Fail to register","");
+    return;
+}
+
+$conn->close();
+
+?>
